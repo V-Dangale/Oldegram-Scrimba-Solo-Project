d586e20910d1aacc1a605a57c8606c8ea695f2cb
Vhutali <120512163+V-Dangale@users.noreply.github.com>
2026-08-17 13:05:53 +0200

    updated js
    refactored to have data in one file as rest of js

--- index.js
+++ index.js
@@ -1,4 +1,37 @@
-import { posts } from "/data.js";
+// import { posts } from "/data.js";
+const posts = [
+	{
+		name: "Vincent van Gogh",
+		username: "vincey1853",
+		location: "Zundert, Netherlands",
+		avatar: "images/avatar-vangogh.jpg",
+		post: "/images/post-vangogh.jpg",
+		comment: "just took a few mushrooms lol",
+		likes: 21,
+		alt: "Portrait of Vincent Van Gogh as a teen in a black and white suit",
+	},
+	{
+		name: "Gustave Courbet",
+		username: "gus1819",
+		location: "Ornans, France",
+		avatar: "images/avatar-courbet.jpg",
+		post: "/images/post-courbet.jpg",
+		comment: "i'm feelin a bit stressed tbh",
+        likes: 4,
+        alt: "Gustav pulling his hair in shock",
+	},
+	{
+		name: "Joseph Ducreux",
+		username: "jd1735",
+		location: "Paris, France",
+		avatar: "images/avatar-ducreux.jpg",
+		post: "/images/post-ducreux.jpg",
+		comment:
+			"gm friends! which coin are YOU stacking up today?? post below and WAGMI!",
+        likes: 152,
+        alt: "Joseph showing finger guns",
+	},
+];
 
 const mainEL = document.getElementById("main");
 
